a6bcd7521bca2eb5eb118579cd5374baa663f299
pavithra419 <pavithra06a@gmail.com>
2026-03-15 10:55:43 +0530

    Resume-Maker-Web-App2.o
    A colorful and user-friendly Resume Maker web application built using HTML, CSS, and JavaScript. This tool allows users to enter their personal information, skills, experience, and projects to generate a clean and professional resume layout with multiple sections and a profile picture.

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ function generateResume(){
 document.getElementById("rname").innerText =
 document.getElementById("name").value;
 
+document.getElementById("rphone").innerText =
+document.getElementById("phone").value;
+
 document.getElementById("remail").innerText =
 document.getElementById("email").value;
 
-document.getElementById("rphone").innerText =
-document.getElementById("phone").value;
+document.getElementById("raddress").innerText =
+document.getElementById("address").value;
+
+document.getElementById("reducation").innerText =
+document.getElementById("education").value;
 
 document.getElementById("rabout").innerText =
 document.getElementById("about").value;
@@ -15,31 +21,49 @@ document.getElementById("about").value;
 document.getElementById("rskills").innerText =
 document.getElementById("skills").value;
 
-document.getElementById("reducation").innerText =
-document.getElementById("education").value;
+document.getElementById("rexperience").innerText =
+document.getElementById("experience").value;
 
 document.getElementById("rprojects").innerText =
 document.getElementById("projects").value;
 
-document.getElementById("rexperience").innerText =
-document.getElementById("experience").value;
+document.getElementById("rlinkedin").innerText =
+document.getElementById("linkedin").value;
+
+document.getElementById("rlinkedin").href =
+document.getElementById("linkedin").value;
+
+document.getElementById("rgithub").innerText =
+document.getElementById("github").value;
+
+document.getElementById("rgithub").href =
+document.getElementById("github").value;
 
 
-const file =
-document.getElementById("image").files[0];
+// profile image
+const file = document.getElementById("image").files[0];
 
 if(file){
 
 const reader = new FileReader();
 
 reader.onload = function(e){
-
 document.getElementById("profilePic").src = e.target.result;
-
 }
 
 reader.readAsDataURL(file);
 
 }
+
+}
+
+
+function downloadPDF(){
+
+const element = document.getElementById("resume");
+
+html2pdf()
+.from(element)
+.save("resume.pdf");
 
 }
